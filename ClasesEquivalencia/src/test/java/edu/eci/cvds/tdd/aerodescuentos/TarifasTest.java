1d4c3cd45f4c6115a0215c269478ccf2518e405b
unknown <maria.hernandez-v@labinfo.is.escuelaing.edu.co>
2020-02-03 09:30:39 -0500

    actualizacionfin

--- ClasesEquivalencia/src/test/java/edu/eci/cvds/tdd/aerodescuentos/TarifasTest.java
+++ ClasesEquivalencia/src/test/java/edu/eci/cvds/tdd/aerodescuentos/TarifasTest.java
@@ -2,49 +2,62 @@ package edu.eci.cvds.tdd.aerodescuentos;
 
 import org.junit.Assert;
 import org.junit.Test;
-
+import java.lang.Exception;
 public class TarifasTest {
 
     @Test
-    public void validateRate() {
+    public void validateAgeLess() {
 		CalculadorDescuentos calculate= new CalculadorDescuentos();
-		double discount=calculate.calculoTarifa(1000000,22,15);
+		double discount=calculate.calculoTarifa(1000000,8,15);
         Assert.assertTrue(discount!=1000000);
     }
 	
-	@Test
-    public void validateRate2() {
+	 @Test
+    public void validateAgeSame18() {
 		CalculadorDescuentos calculate= new CalculadorDescuentos();
-		double discount=calculate.calculoTarifa(1000000,23,68);
-        Assert.assertTrue(discount!=1000000);
-    }
-	
-	/*@Test
-    public void validateRate3() {
-		CalculadorDescuentos calculate= new CalculadorDescuentos();
-		double discount=calculate.calculoTarifa(-2,22,15);
-        Assert.assertTrue(discount==1000000);
-    }*/
-	
-	@Test
-    public void validateRate3() {
-		CalculadorDescuentos calculate= new CalculadorDescuentos();
-		double discount=calculate.calculoTarifa(1000000,15,18);
+		double discount=calculate.calculoTarifa(1000000,8,18);
         Assert.assertTrue(discount==1000000);
     }
-	/*
+	
 	@Test
-    public void validateRate() {
+    public void validateAgeGreater() {
 		CalculadorDescuentos calculate= new CalculadorDescuentos();
-		double discount=calculate.calculoTarifa(1000000,22,15);
+		double discount=calculate.calculoTarifa(1000000,5,68);
         Assert.assertTrue(discount!=1000000);
     }
 	
 	@Test
-    public void validateRate() {
+    public void validateAgeMiddle() {
 		CalculadorDescuentos calculate= new CalculadorDescuentos();
-		double discount=calculate.calculoTarifa(1000000,22,15);
+		double discount=calculate.calculoTarifa(1000000,5,30);
+        Assert.assertTrue(discount==1000000);
+    }
+	
+	@Test
+    public void validateAgeSame65() {
+		CalculadorDescuentos calculate= new CalculadorDescuentos();
+		double discount=calculate.calculoTarifa(1000000,5,65);
+        Assert.assertTrue(discount==1000000);
+    }
+	
+	@Test
+    public void validateDaysLess() {
+		CalculadorDescuentos calculate= new CalculadorDescuentos();
+		double discount=calculate.calculoTarifa(1000000,15,25);
+        Assert.assertTrue(discount==1000000);
+    }
+	
+	@Test
+    public void validateDaysSame() {
+		CalculadorDescuentos calculate= new CalculadorDescuentos();
+		double discount=calculate.calculoTarifa(1000000,20,25);
+        Assert.assertTrue(discount==1000000);
+    }
+	
+	@Test
+    public void validateDaysGreater() {
+		CalculadorDescuentos calculate= new CalculadorDescuentos();
+		double discount=calculate.calculoTarifa(1000000,22,25);
         Assert.assertTrue(discount!=1000000);
-    }*/
-
+    }
 }
